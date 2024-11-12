42ca3dc26784f7a4298d652fc2c07b3ed1da5036
Joel Okoromi <okmarq@gmail.com>
2024-11-12 19:10:12 +0100

    update helper

--- app/helpers.php
+++ app/helpers.php
@@ -7,28 +7,17 @@ use Illuminate\Support\Facades\Log;
 if (!function_exists('runBackgroundJob')) {
     function runBackgroundJob(BackgroundJob $job): void
     {
-        $pid = null;
         try {
             (new JobValidator())->validate($job->class, $job->method);
             /*
              * php artisan background:run <job id>
-             * php artisan background:run <job id>
              */
-            $output = shell_exec("start /B " . 'php -v');
-            Log::critical($output);
-            $cmd = "php " . base_path("artisan") . " background:run " . $job->id . " > /dev/null 2>&1 & echo $!";
+            $cmd = "php " . base_path("artisan") . " background:run " . $job->id;
             // Run command in a background process
-            if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                $pid = shell_exec("start /B " . $cmd);
-            } else {
-                $pid = shell_exec($cmd);
-            }
-            Log::debug("Running command: $cmd");
-            Log::debug("Command output: $pid");
+            strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? shell_exec("start /B " . $cmd) : shell_exec($cmd);
         } catch (Exception $e) {
             Log::error('validation failed: ' . $e->getMessage(), $e->getTrace());
             Log::channel('background_jobs_errors')->error("Job {$job->id} failed: {$job->class}@{$job->method}", [
-                'pid' => $pid,
                 'parameters' => json_decode($job->parameters),
                 'error' => $e->getMessage(),
                 'status' => config('constants.status.failed')
@@ -42,10 +31,7 @@ if (!function_exists('cancelJob')) {
     function cancelJob($pid): void
     {
         // Terminate the process based on OS
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            exec("taskkill /F /PID $pid");
-        } else {
-            exec("kill -9 $pid");
-        }
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') exec("taskkill /F /PID $pid");
+        else exec("kill -9 $pid");
     }
 }
