fc184b0ba4399761b822281f23112a26bc5051e4
Joel Okoromi <okmarq@gmail.com>
2024-11-12 14:50:16 +0100

    add middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return !Auth::check() || !Auth::user()->hasRole(config('constants.role.admin')) ? Redirect::route('dashboard') : $next($request);
+    }
+}
